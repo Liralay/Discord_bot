583aff50c5c6cbf7453e231d21d124c0ff0e56dd
Liralay <mlp.charley@gmail.com>
2024-03-13 22:32:52 +0300

    init repo

---
+++ main.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+client = OpenAI(
+  organization=os.getenv("Personal"),
+)
+
+stream = client.chat.completions.create(
+    model="gpt-3.5-turbo-0125",
+    messages=[{"role": "user", "content": "Say this is a test"}],
+    stream=True,
+)
+for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end="")
