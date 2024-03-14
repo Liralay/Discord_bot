51edc9b4b843a0f99cc09d6b1d3222bffd6f6f51
Liralay <mlp.charley@gmail.com>
2024-03-14 19:51:10 +0300

    fixed bot misspells

--- discord_bot.py
+++ discord_bot.py
@@ -18,12 +18,12 @@ client = commands.Bot(command_prefix='!', intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'Bot has logged in as {bot.user}')
+    print(f'Bot has logged in as {client.user}')
 
 @client.event
 async def on_message(message):
     # Не отвечать на сообщения от самого бота
-    if message.author == bot.user:
+    if message.author == client.user:
         return
 
     # Эхо-ответ: повторяем сообщение пользователя
