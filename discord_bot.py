48d79f8c57768112f1a6b5e1c3f99e1cd5a7833e
Liralay <mlp.charley@gmail.com>
2024-03-14 22:05:33 +0300

    some optimzation and added fiture to ignore Serega

--- discord_bot.py
+++ discord_bot.py
@@ -1,10 +1,16 @@
 import os
 from dotenv import load_dotenv
-from openai import OpenAI
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_API_KEY')
 
+
+
+from openai import OpenAI
+
+client_chat_gpt = OpenAI(
+  organization=os.getenv("Personal"),
+)
 
 
 import discord
@@ -19,38 +25,36 @@ client = commands.Bot(command_prefix='/', intents=intents)
 
 
 @client.event
-@client.event
 async def on_ready():
     print(f'Logged in as {client.user.name}')
 
-
-@client.command(name='gpt')
-async def echo(ctx, *, arg):
-
-    client = OpenAI(
-  organization=os.getenv("Personal"),
-)
+@client.command(name = 'help')
+async def help(ctx):
+     await ctx.send('') #TODO Спросить у сереги "Что ты умеешь" и его ответ записать как дефолтный ответ бота
 
 
-    message = arg
-    stream = client.chat.completions.create(
-    model="gpt-3.5-turbo-0125",
-    messages=[{"role": "user", "content": message}], # Serega LOX
-    stream=True,
-)   
-    data = ''
-    for chunk in stream:
-        if chunk.choices[0].delta.content is not None:
-            
-            data = data + (chunk.choices[0].delta.content)
-            print(data)
-    if data:
-            await ctx.send(data)
+@client.command(name = 'gpt')
+async def gpt(ctx, *, arg):
+    serega_id = 252109360526786562
+    if ctx.author.id != serega_id:
+        message = arg
+        stream = client_chat_gpt.chat.completions.create(
+        model="gpt-3.5-turbo-0125",
+        messages=[{"role": "user", "content": message}], # Serega LOX
+        stream=True,
+    )   
+        data = []
+        for chunk in stream:
+            if chunk.choices[0].delta.content is not None:
+                
+                data.append(chunk.choices[0].delta.content)
+                data_string = ''.join(map(str, data))
+        if data:
+                await ctx.send(data_string)
+        else:
+                await ctx.send('error')
     else:
-            await ctx.send('error')
-
-
-
+         await ctx.send('Я сам с собой не общаюсь')
 
 
 # Запустить бота с вашим токеном
