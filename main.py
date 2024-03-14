03883b26625b20d9df46e21cb6f526e1e5931b87
Liralay <mlp.charley@gmail.com>
2024-03-14 22:59:21 +0300

    added admin id checkup

--- main.py
+++ main.py
@@ -5,14 +5,6 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_API_KEY')
 
 
-
-from openai import OpenAI
-
-client_chat_gpt = OpenAI(
-  organization=os.getenv("Personal"),
-)
-
-
 import discord
 from discord.ext import commands
 
@@ -23,20 +15,43 @@ intents.message_content = True
 # Создаём объект бота
 client = commands.Bot(command_prefix='/', intents=intents)
 
+serega_alive = False
+serega_id = 234258286533148673
+
+admin_id = [242344024726437888, 252109360526786562]
+
+
+usage = {}
+
+
+from openai import OpenAI
+
+client_chat_gpt = OpenAI(
+  organization=os.getenv("Personal"),
+)
+
 
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user.name}')
 
-@client.command(name = 'help')
+@client.command(name = 'who_am_i')
 async def help(ctx):
      await ctx.send('') #TODO Спросить у сереги "Что ты умеешь" и его ответ записать как дефолтный ответ бота
 
+@client.command(name = 'serega')
+async def serega(ctx):
+    if ctx.author.id in admin_id:     
+        global serega_alive 
+        serega_alive = not serega_alive
+        await ctx.send(f'Done. serega_alive set to {serega_alive}')
+    else:
+        await ctx.send('C лохами не общаюсь (сам с собой не общаюсь)')
 
 @client.command(name = 'gpt')
 async def gpt(ctx, *, arg):
-    serega_id = 252109360526786562
-    if ctx.author.id != serega_id:
+    
+    if serega_alive == True or ctx.author.id != serega_id:
         message = arg
         stream = client_chat_gpt.chat.completions.create(
         model="gpt-3.5-turbo-0125",
@@ -57,5 +72,5 @@ async def gpt(ctx, *, arg):
          await ctx.send('Я сам с собой не общаюсь')
 
 
-# Запустить бота с вашим токеном
-client.run(TOKEN)
+if __name__ == '__main__':
+    client.run(TOKEN)
